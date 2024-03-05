4eb4ecefdb1431a0957e6693ac82c5dbec0cc2e4
splanaspe <splanaspe@uoc.edu>
2024-03-05 22:17:04 +0100

    Primer commit del proyecto de backend con java, muy contentos de poder haber hecho la primera ruta de la rest api

---
+++ src/main/java/com/main/restservice/AppBackendJavaApplication.java
@@ -0,0 +1,14 @@
+package com.main.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AppBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/main/restservice/Greeting.java
@@ -0,0 +1,3 @@
+package com.main.restservice;
+
+public record Greeting(long id, String content) { }
